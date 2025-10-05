4944363e36ff74b5ac98371459accdeab361f87d
zinx110 <tasdid110@gmail.com>
2025-10-06 02:28:10 +0600

    110. Balanced Binary Tree

--- 110. Balanced Binary Tree/sol.go
+++ 110. Balanced Binary Tree/sol.go
@@ -8,37 +8,43 @@ package BalancedBinaryTree
  *     Right *TreeNode
  * }
  */
- func isBalanced(root *TreeNode) bool {
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
 
-    var dfs func(node *TreeNode) int 
-    dfs = func(node *TreeNode) int {
-        if node == nil {
-            return 0
-        }
-        left := dfs(node.Left)
-        right := dfs(node.Right)
-        if left < 0 || right < 0 || abs(left - right) > 1 {
-            return -1
-        }
-        return 1 + max(left, right)
-    }
+func isBalanced(root *TreeNode) bool {
 
-    depth := dfs(root)
+	var dfs func(node *TreeNode) int
+	dfs = func(node *TreeNode) int {
+		if node == nil {
+			return 0
+		}
+		left := dfs(node.Left)
+		right := dfs(node.Right)
+		if left < 0 || right < 0 || abs(left-right) > 1 {
+			return -1
+		}
+		return 1 + max(left, right)
+	}
 
-    return depth >= 0 
-    
+	depth := dfs(root)
+
+	return depth >= 0
+
 }
 
 func max(a, b int) int {
-    if a > b {
-        return a
-    }
-    return b
+	if a > b {
+		return a
+	}
+	return b
 }
 
 func abs(a int) int {
-    if a < 0 {
-        return -a
-    }
-    return a
+	if a < 0 {
+		return -a
+	}
+	return a
 }
--- 110. Balanced Binary Tree/sol.py
+++ 110. Balanced Binary Tree/sol.py
@@ -1,5 +1,7 @@
+from typing import Optional
+
 class TreeNode:
-    def __init__(self, val = 0, left = Optional[TreeNode], right = Optional[TreeNode]):
+    def __init__(self, val: int = 0, left: 'Optional[TreeNode]' = None, right: 'Optional[TreeNode]' = None):
         self.val = val
         self.left = left
         self.right = right
@@ -22,4 +24,5 @@ class Solution:
             return False
         return True
 
+        
         
