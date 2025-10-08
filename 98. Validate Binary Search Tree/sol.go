f11f5aca92c402bc73c0435ab4a3be129e39c138
zinx110 <tasdid110@gmail.com>
2025-10-09 01:54:27 +0600

    98. Validate Binary Search Tree (added bfs to golang solution)

--- 98. Validate Binary Search Tree/sol.go
+++ 98. Validate Binary Search Tree/sol.go
@@ -22,3 +22,57 @@ func isValidBST(root *TreeNode) bool {
 	}
 	return dfs(root, nil, nil)
 }
+
+// BFS
+func isValidBSTBFS(root *TreeNode) bool {
+	que := deque{}
+	que.append(root, nil, nil)
+	for len(que.nodes) > 0 {
+		node, low, high := que.popleft()
+		if low != nil && *low >= node.Val {
+			return false
+		}
+		if high != nil && *high <= node.Val {
+			return false
+		}
+		if node.Left != nil {
+			que.append(node.Left, low, &node.Val)
+		}
+		if node.Right != nil {
+			que.append(node.Right, &node.Val, high)
+		}
+
+	}
+
+	return true
+
+}
+
+type deque struct {
+	nodes []que
+}
+
+type que struct {
+	node *TreeNode
+	low  *int
+	high *int
+}
+
+func (q *deque) init() *deque {
+	q.nodes = make([]que, 0)
+	return q
+}
+
+func (q *deque) append(node *TreeNode, low, high *int) {
+
+	q.nodes = append(q.nodes, que{node, low, high})
+}
+
+func (q *deque) popleft() (*TreeNode, *int, *int) {
+	if len(q.nodes) > 0 {
+		val := q.nodes[0]
+		q.nodes = q.nodes[1:]
+		return val.node, val.low, val.high
+	}
+	return nil, nil, nil
+}
