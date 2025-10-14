b1f24493a8ece62da5a82dae7d70bd959dc4a13d
zinx110 <tasdid110@gmail.com>
2025-10-14 11:19:12 +0600

    added run test to 208

--- 208. Implement Trie (Prefix Tree)/sol.go
+++ 208. Implement Trie (Prefix Tree)/sol.go
@@ -1,4 +1,12 @@
-package implementTriePrefixTree
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+	"reflect"
+)
 
 type TrieNode struct {
 	children  map[rune]*TrieNode
@@ -48,4 +56,74 @@ func (this *Trie) StartsWith(prefix string) bool {
 	}
 	return true
 
+}
+
+func main() {
+	// currPath, _ := os.Getwd()
+	// fmt.Println("Current path: ", currPath)
+
+	filename := "./208. Implement Trie (Prefix Tree)/testcase.json"
+	path := filepath.Join(filename)
+	// fmt.Printf("Filepath: %v", path)
+
+	jsonData, err := os.ReadFile(path)
+	if err != nil {
+		fmt.Printf("error reading file: %v", err)
+		return
+	}
+
+	// fmt.Printf("File data: %v, type %T", string(jsonData), jsonData)
+	var testCase TestCase
+	err = json.Unmarshal(jsonData, &testCase)
+	if err != nil {
+		fmt.Printf("error unmarshalling file: %v", err)
+		return
+	}
+	passNum := 0
+
+	for _, test := range testCase.Tests {
+		trie := Constructor()
+		input := test.Input
+		commands := input.Commands
+		arguments := input.Arguments
+		expectedOutput := test.ExpectedOutput
+		output := []any{}
+		for i := 0; i < len(commands); i++ {
+			res := any(nil)
+			if commands[i] == "Trie" {
+				trie = Constructor()
+			} else if commands[i] == "insert" {
+				arg := arguments[i][0]
+				trie.Insert(arg)
+			} else if commands[i] == "search" {
+				arg := arguments[i][0]
+				res = trie.Search(arg)
+			} else if commands[i] == "startsWith" {
+				arg := arguments[i][0]
+				res = trie.StartsWith(arg)
+			}
+			output = append(output, res)
+		}
+		if !reflect.DeepEqual(output, expectedOutput) {
+			fmt.Printf("Test failed")
+		} else {
+			passNum++
+			fmt.Printf("Test passed")
+		}
+		fmt.Printf("\nInput: \n  Arguments: %v \n  Commands: %v \n  Output: %v \n  Expected Output: %v \n", arguments, commands, output, expectedOutput)
+		fmt.Println("--------------------------------")
+	}
+	fmt.Printf("Total tests: %v, Passed tests: %v, Failed tests: %v", len(testCase.Tests), passNum, len(testCase.Tests)-passNum)
+}
+
+type Input struct {
+	Commands  []string   `json:"commands"`
+	Arguments [][]string `json:"arguments"`
+}
+type Test struct {
+	Input          Input `json:"input"`
+	ExpectedOutput []any `json:"expectedOutput"`
+}
+type TestCase struct {
+	Tests []Test `json:"tests"`
 }
